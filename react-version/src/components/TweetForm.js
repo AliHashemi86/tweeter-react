951d9a249a246385ca4409d5b7754c26da3f4334
Ali Hashemi <alihashemmi.ca@gmail.com>
2022-02-18 19:47:36 -0500

    add event handling

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -1,9 +1,20 @@
 function TweetForm(){
+  const ask = "What are you humming about?"
+  const submitButton=(e)=> {
+    e.preventDefault();
+  }
+  const submitForm = (e)=> {
+    e.preventDefault();
+  }
+
   return (
     <section class="newtweet">
       <form method="post" action="/tweets" class="newtweet__form">
-        <textarea class="form__textarea" name="text" placeholder="What are you humming about?"></textarea>
-        <input type="submit" value="Tweet" class="form__input" />
+        <textarea class="form__textarea" name="text" placeholder={ask}></textarea>
+        <input type="submit" value="Tweet" class="form__input" 
+        onClick={(e)=> submitButton(e)}
+        onSubmit={(e)=> submitForm(e)}
+        />
         <span class="form__counter">140</span>
       </form>
     </section>
